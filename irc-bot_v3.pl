0c00aaf5c2396ece77754637deb5542716230d29
dgoncalvesramos <david.goncalves.ramos@gmail.com>
2022-05-21 11:54:25 +0200

    Complete challenge 3

---
+++ irc-bot_v3.pl
@@ -0,0 +1,69 @@
+use strict;
+use warnings;
+# On utilise la librairie Net::IRC pour se connecter à IRC
+use Net::IRC ;
+# On utilise la libraire Mime pour décoder la chaine de caractère
+use MIME::Base64;
+
+# Configuration des options de connexion (serveur, login) :
+my $server = 'irc.root-me.org';
+my $nick = 'perlBot_v1';
+my $time = 0 ;
+
+# Informations concernant le Bot :
+my $ircname = 'irc.root-me.org';
+my $username = 'perlBot';
+my $version = '1.0';
+
+# Channel sur lequel on veut que le Bot aille :
+my $channel = '#root-me_challenge';
+
+# On crée l'objet qui nous permet de nous connecter à IRC :
+my $irc = new Net::IRC;
+
+# On crée l'objet de connexion à IRC :
+my $conn = $irc->newconn(
+    'Server'      => $server,
+    'Port'        => 6667, 
+    'Nick'        => $nick,
+    'Ircname'     => $ircname,
+    'Username'    => $username
+);
+
+# On installe les fonctions de Hook :
+$conn->add_handler('376', \&on_connect);         # Fin du MOTD => on est connecté
+$conn->add_handler('msg', \&on_private); 	   # En privé 
+ 
+ 
+ 
+# On lance la connexion et la boucle de gestion des événements :
+$irc->start();
+
+
+sub on_connect
+{
+    my ($conn, $event) = @_;
+    
+    $conn->join($channel);
+    $conn->privmsg('Candy', '!ep3');
+    $conn->{'connected'} = 1;
+} # Fin on_connect
+
+
+sub on_private
+{
+    my ($conn, $event) = @_;
+    my $text = $event->{'args'}[0];
+    if($time==0) 
+    {
+    	$text =~ tr/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM/;
+    	$conn->privmsg('Candy', "!ep3 -rep $text");
+    }
+    else
+    {
+        $conn->print("PRIVE<" . $event->nick() . ">\t| $text");
+    }
+    $time++;
+    	
+} # Fin on_private
+
